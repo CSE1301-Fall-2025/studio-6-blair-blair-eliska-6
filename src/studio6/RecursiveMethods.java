53d313d200c77fc0466095ce506897653427e858
blair gipe <g.rileigh@wustl.edu>
2025-10-22 14:03:08 -0500

    b

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -4,19 +4,17 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class RecursiveMethods {
 
-	/**
-	 * Computes the geometric sum for the first n terms in the series
-	 * 
-	 * @param n the number of terms to include in the sum
-	 * @return the sum of the first n terms of the geometric series (1/2 + 1/4 + 1/8
-	 *         ...)
-	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
-		
+    // Base case
+    if (n == 0) {
+        return 0.0;
 	}
+    else
+    return Math.pow(0.5, n) + geometricSum(n - 1);
+	}
+	
+		
+	
 
 	
 	/**
@@ -40,8 +38,27 @@ public class RecursiveMethods {
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	 */
-	public static int[] toReversed(int[] array) {
+	public static int toReversed(int[] array) {
+		int[] reversed = new int[array.length];
+		if (array.length == 0) 
+		{ return array.length; }
+
+		Else 
+
 		
+			int lastIndex = array.length - 1;
+			for (int index = 0; index <= array.length / 2; ++index) {
+				int mirrorIndex = lastIndex - index;
+				// note:
+				// since we read from array and write to reversed
+				// we do not need to use a temp variable
+				reversed[index] = array[mirrorIndex];
+				reversed[mirrorIndex] = array[index];
+			}
+		}
+		return reversed;
+	}
+}	
 			// FIXME create a helper method that can recursively reverse the given array
 			return new int[0];
 		
